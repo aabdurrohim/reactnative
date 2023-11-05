4bc21ac9efea459f7c36961ea949cb46a1347c00
aabdurrohim <abdurrohim.i.p@gmail.com>
2023-11-05 19:29:24 +0700

    update

---
+++ data/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    imageUrl: "https://i.pinimg.com/564x/07/d6/9c/07d69c9c2b3a92524ef4e4461b1801e7.jpg",
+    nama: "Nis",
+    nim: "21120120120001",
+  },
+  {
+    id: 2,
+    imageUrl: "https://i.pinimg.com/564x/07/d6/9c/07d69c9c2b3a92524ef4e4461b1801e7.jpg",
+    nama: "Leo",
+    nim: "211212233445",
+  },
+  {
+    id: 3,
+    imageUrl: "https://i.pinimg.com/564x/07/d6/9c/07d69c9c2b3a92524ef4e4461b1801e7.jpg",
+    nama: "Roy",
+    nim: "21121124455667",
+  },
+  {
+    id: 4,
+    imageUrl: "https://i.pinimg.com/564x/07/d6/9c/07d69c9c2b3a92524ef4e4461b1801e7.jpg",
+    nama: "Ab",
+    nim: "21121124455667",
+  },
+];
---
+++ pages/HomeScreen.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
+
+function HomeScreen() {
+  const [trending, setTrending] = useState([]);
+  const [all, setAll] = useState([]);
+  const [loadingTrending, setLoadingTrending] = useState(true);
+  const [loadingAll, setLoadingAll] = useState(true);
+  const [errorTrending, setErrorTrending] = useState(null);
+  const [errorAll, setErrorAll] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const trendingResponse = await fetch("https://api.jikan.moe/v4/top/anime");
+        if (trendingResponse.ok) {
+          const trendingData = await trendingResponse.json();
+          setTrending(trendingData.data);
+          setLoadingTrending(false);
+        } else {
+          setErrorTrending("Failed to fetch data");
+
+          setLoadingTrending(false);
+        }
+
+        const allResponse = await fetch("https://api.jikan.moe/v4/top/anime?filter=bypopularity");
+        if (allResponse.ok) {
+          const allData = await allResponse.json();
+          setAll(allData.data);
+          setLoadingAll(false);
+        } else {
+          setErrorAll("Failed to fetch data");
+          setLoadingAll(false);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View>
+      <View>
+        <Text
+          style={{
+            fontSize: 35,
+            fontWeight: "bold",
+            color: "black",
+            marginLeft: 10,
+            marginTop: 40,
+            marginBottom: 10,
+          }}
+        >
+          Trending
+        </Text>
+      </View>
+      {loadingTrending ? (
+        <ActivityIndicator />
+      ) : errorTrending ? (
+        <Text>Something went wrong :(</Text>
+      ) : (
+        <ScrollView horizontal={true}>
+          {trending.map((trend) => (
+            <View style={{ width: 120, margin: 10 }}>
+              <Image source={{ uri: trend.images.jpg.image_url }} style={{ height: 200, borderRadius: 15 }} />
+              <Text
+                style={{
+                  fontSize: 14,
+                  fontWeight: "bold",
+                  marginTop: 5,
+                  marginBottom: 75,
+                  textAlign: "center",
+                }}
+                numberOfLines={2}
+              >
+                {trend.title}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+      )}
+
+      <View>
+        <Text
+          style={{
+            fontSize: 35,
+            fontWeight: "bold",
+            color: "black",
+            marginLeft: 10,
+            marginTop: 5,
+            marginBottom: 10,
+          }}
+        >
+          All Anime
+        </Text>
+      </View>
+
+      {loadingAll ? (
+        <ActivityIndicator />
+      ) : errorAll ? (
+        <Text>Something went wrong :(</Text>
+      ) : (
+        <FlatList
+          data={all}
+          keyExtractor={(item) => (item.malId ? item.malId.toString() : "")}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: "row", alignItems: "top", margin: 10 }}>
+              <Image source={{ uri: item.images.jpg.image_url }} style={{ height: 200, width: 150, borderRadius: 15 }} />
+              <View style={{ marginLeft: 10 }}>
+                <Text style={{ fontSize: 24, fontWeight: "bold" }}>{item.title}</Text>
+                <Text>Score: {item.score}</Text>
+              </View>
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
+}
+
+export default HomeScreen;
